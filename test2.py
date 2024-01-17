0a416939e7757ff09f798616248527c73c0003e9
jf-grpo <j7f7r7@gmail.com>
2024-01-17 17:18:23 -0600

    installed locally. Auth test failed

---
+++ test2.py
@@ -0,0 +1,16 @@
+from pydomo import Domo
+
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+client_id = os.getenv("CLIENT_ID")
+secret = os.getenv("SECRET")
+api_host = os.getenv("API_HOST")
+
+
+domo = Domo(client_id, secret, api_host)
+
+all_users = domo.users.list(10, 0)
+print(all_users)
